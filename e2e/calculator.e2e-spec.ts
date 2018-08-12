8a41f206f45e7ca1379d46f36d4e5144496652bd
AndreasMN <andycutter@godmail.dk>
2018-08-12 20:06:29 +0200

    (MsM) add e2e tests, all go green. Split up expected texts in "fill in form"-test to make it pass

--- e2e/calculator.e2e-spec.ts
+++ e2e/calculator.e2e-spec.ts
@@ -2,17 +2,25 @@ import { by, element , browser} from 'protractor';
 
 describe('Calculator', () => {
     it('fill in form', () => {
-        element(by.css('[aria-labelledby="lbl-0"]')).sendKeys(100);
+        element(by.css('[aria-labelledby="lbl-0"]')).sendKeys(110);
         element(by.css('[aria-labelledby="lbl-1"]')).sendKeys(200);
         element(by.buttonText('Calculate')).click();
         browser.sleep(1000);
 
         expect(element(by.css('.card-content'))
             .getText())
-            .toContain('Person: Weight 100 kg, Height 200 cm')
+            .toContain('Person weight: 110 kg')
 
         expect(element(by.css('.card-content'))
             .getText())
             .toContain('You are overweight')
+
+        expect(element(by.css('.card-content'))
+            .getText())
+            .toContain('Person height: 200 cm')
+
+        expect(element(by.css('.card-content'))
+            .getText())
+            .toContain('BMI: 27.5')           
     });
 });
